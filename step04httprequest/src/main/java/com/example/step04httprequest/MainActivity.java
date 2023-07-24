28f2aaba84ca99683f4dd0d798235faf230477c9
kimgura <krkrdudwn@naver.com>
2023-07-24 11:12:46 +0900

    Android step04Httprequest2 , example 모듈 추가함

--- step04httprequest/src/main/java/com/example/step04httprequest/MainActivity.java
+++ step04httprequest/src/main/java/com/example/step04httprequest/MainActivity.java
@@ -41,31 +41,31 @@ public class MainActivity extends AppCompatActivity {
         // 백그라운드(새로운 스레드의 run() 메소드)에서 작업할 내용을 이 메소드에서 작업하면 된다.
         @Override
         protected String doInBackground(String... strings) {
-            //문자열을 누적시킬 객체
+            // 문자열을 누적 시킬 객체
             StringBuilder builder=new StringBuilder();
 
-            //strings 의 0 번방에 요청 url 이 들어 있다.
+            // strings 의 0 번방에 요청 url 이 들어 있다.
             try {
-                //요청 url 을 생성자의 인자로 전달해서 URL 객체를 생성한다.
+                // 요청 url 을 생성자의 인자로 전달해서 URL 객체를 생성한다.
                 URL url = new URL(strings[0]);
-                //URLConnection 객체를 원래 type (자식 type) 으로 casting 해서 받는다.
+                // URLConnection 객체를 원래 type (자식 type) 으로 casting 해서 받는다.
                 HttpURLConnection conn=(HttpURLConnection) url.openConnection();
-                //정상적으로 연결이 되었다면
+                // 정상적으로 연결이 되었다면
                 if(conn != null){
-                    conn.setConnectTimeout(20000); //응답을 기다리는 최대 대기 시간
-                    conn.setRequestMethod("GET");// 요청 메소드 설정 (Default 는 GET)
-                    conn.setUseCaches(false);//케쉬 사용 여부
-                    //응답 코드를 읽어온다.
+                    conn.setConnectTimeout(20000); // 응답을 기다리는 최대 대기 시간
+                    conn.setRequestMethod("GET"); // 요청 메소드 설정 (Default 는 GET)
+                    conn.setUseCaches(false); // 캐쉬 사용 여부
+                    // 응답 코드를 읽어온다.
                     int responseCode=conn.getResponseCode();
-                    if(responseCode == HttpURLConnection.HTTP_OK){ //정상 응답이라면
-                        //문자열을 읽어들일수 있는 객체의 참조값 얻어오기
+                    if(responseCode == HttpURLConnection.HTTP_OK){ // 정상 응답이라면
+                        // 문자열을 읽어들일 수 있는 객체의 참조값 얻어오기
                         BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                        //반복문 돌면서
+                        // 반복문 돌면서
                         while(true){
-                            //문자열을 한줄씩 읽어 들인다.
+                            // 문자열을 한줄씩 읽어 들인다.
                             String line=br.readLine();
                             if(line==null)break;
-                            //StringBuilder 객체에 읽어들인 문자열을 누적 시킨다.
+                            // StringBuilder 객체에 읽어들인 문자열을 누적 시킨다.
                             builder.append(line);
                         }
                     }
@@ -73,10 +73,10 @@ public class MainActivity extends AppCompatActivity {
             }catch (Exception e){
                 Log.e("RequestTask 클래스", e.getMessage());
             }
-            //StringBuilder 객체에 누적된 문자열을 String type 으로 한번에 얻어내서 리턴해 준다.
+            // StringBuilder 객체에 누적된 문자열을 String type 으로 한번에 얻어내서 리턴해 준다.
             return builder.toString();
         }
-        // doInBackground() 메소드에서 리턴된 문자열이 이 메소드가 호출되면서 메소드의 인자로 전달된다.
+        // doInBackground() 메소드에서 리턴된 문자열이 이 메소드가 호출 되면서 메소드의 인자로 전달된다.
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
